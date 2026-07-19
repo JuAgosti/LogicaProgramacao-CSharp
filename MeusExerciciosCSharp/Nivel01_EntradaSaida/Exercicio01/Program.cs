3f839baef6623dc74806f80404432ecb8768f1e0
Julia Agosti <juliaagosti30@gmail.com>
2026-07-19 14:42:35 -0300

    feat: conclui o exercicio 01

--- MeusExerciciosCSharp/Nivel01_EntradaSaida/Exercicio01/Program.cs
+++ MeusExerciciosCSharp/Nivel01_EntradaSaida/Exercicio01/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio01;
+class Exercicio01
+{
+    public static void Executar()
+    {
+        double quantMinima;
+        double quantMaxima;
+        double estoqueMedia;
+
+        Console.WriteLine("Digite a quantidade mínima do estoque: ");
+        quantMinima = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a quantidade máxima do estoque: ");
+        quantMaxima = double.Parse(Console.ReadLine());
+
+        estoqueMedia = (quantMinima + quantMaxima) / 2;
+
+        Console.WriteLine("Estoque médio: " + estoqueMedia);
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("SELECIONE O EXERCÍCIO QUE DESEJA TESTAR");
+        Console.Write("Digite o número do exercício: ");
+        string opcao = Console.ReadLine();
+
+        Console.WriteLine("\n---------------------------------------------");
+
+        switch (opcao)
+        {
+            case "1":
+                MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio01.Exercicio01.Executar();
+                break;
+            case "2":
+                // MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio02.Exercicio02.Executar();
+                Console.WriteLine("Exercício 2 ainda não foi implementado.");
+                break;
+            default:
+                Console.WriteLine("Opção inválida ou exercício não encontrado.");
+                break;
+        }
+
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine("\nPrograma finalizado. Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
