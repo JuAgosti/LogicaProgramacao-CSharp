a2e8b7cbb0cc090be76db64f5169c9900899ed74
Julia Agosti <juliaagosti30@gmail.com>
2026-07-22 21:17:22 -0300

    feat: conclui o exercicio 19

---
+++ MeusExerciciosCSharp/Nivel02_EstruturasCondicionais/Exercicio19/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio19;
+
+class Exercicio19
+{
+    public static void Executar()
+    {
+        int[] lados = new int[3];
+
+        Console.WriteLine("--- CLASSIFICAÇÃO DE TRIÂNGULO ---");
+        
+        for(int i = 0; i < lados.Length; i++)
+        {
+            Console.WriteLine($"Digite o lado {i + 1}: ");
+            lados[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Validação para verificar se é um triângulo
+        if (lados[0] + lados[1] > lados[2] &&
+            lados[0] + lados[2] > lados[1] &&
+            lados[1] + lados[2] > lados[0]
+            )
+        {
+            if (lados[0] == lados[1] && lados[1] == lados[2] && lados[0] == lados[2])
+            {
+                Console.WriteLine("Triângulo Equilátero!");
+            }
+            else if (lados[0] == lados[1] || lados[1] == lados[2] || lados[0] == lados[2])
+            {
+                Console.WriteLine("Triângulo Isósceles!");
+            }
+            else
+            {
+                Console.WriteLine("Triângulo Escaleno!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Os valores não representam um triângulo.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,74 +4,90 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("SELECIONE O EXERCÍCIO QUE DESEJA TESTAR");
-        Console.Write("Digite o número do exercício: ");
-        string opcao = Console.ReadLine();
+        bool continuar;
 
-        Console.WriteLine("\n---------------------------------------------");
-
-        switch (opcao)
+        do
         {
-            case "1":
-                MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio01.Exercicio01.Executar();
-                break;
-            case "2":
-                MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio02.Exercicio02.Executar();
-                break;
-            case "3":
-                MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio03.Exercicio03.Executar();
-                break;
-            case "4":
-                MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio04.Exercicio04.Executar();
-                break;
-            case "5":
-                MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio05.Exercicio05.Executar();
-                break;
-            case "6":
-                MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio06e07.Exercicio06e07.Executar();
-                break;
-            case "7":
-                MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio06e07.Exercicio06e07.Executar();
-                break;
-            case "8":
-                MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio08.Exercicio08.Executar();
-                break;
-            case "9":
-                MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio09.Exercicio09.Executar();
-                break;
-            case "10":
-                MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio10.Exercicio10.Executar();
-                break;
-            case "11":
-                MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio11.Exercicio11.Executar();
-                break;
-            case "12":
-                MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio12.Exercicio12.Executar();
-                break;
-            case "13":
-                MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio13.Exercicio13.Executar();
-                break;
-            case "14":
-                MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio14.Exercicio14.Executar();
-                break;
-            case "15":
-                MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio15.Exercicio15.Executar();
-                break;
-            case "16":
-                MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio16.Exercicio16.Executar();
-                break;
-            case "17":
-                MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio17.Exercicio17.Executar();
-                break;
-            case "18":
-                MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio18.Exercicio18.Executar();
-                break;
-            default:
-                Console.WriteLine("Opção inválida ou exercício não encontrado.");
-                break;
-        }
+            Console.Clear();
 
-        Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("SELECIONE O EXERCÍCIO QUE DESEJA TESTAR");
+            Console.Write("Digite o número do exercício: ");
+            string opcao = Console.ReadLine();
+
+            Console.WriteLine("\n---------------------------------------------");
+
+            switch (opcao)
+            {
+                case "1":
+                    MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio01.Exercicio01.Executar();
+                    break;
+                case "2":
+                    MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio02.Exercicio02.Executar();
+                    break;
+                case "3":
+                    MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio03.Exercicio03.Executar();
+                    break;
+                case "4":
+                    MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio04.Exercicio04.Executar();
+                    break;
+                case "5":
+                    MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio05.Exercicio05.Executar();
+                    break;
+                case "6":
+                    MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio06e07.Exercicio06e07.Executar();
+                    break;
+                case "7":
+                    MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio06e07.Exercicio06e07.Executar();
+                    break;
+                case "8":
+                    MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio08.Exercicio08.Executar();
+                    break;
+                case "9":
+                    MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio09.Exercicio09.Executar();
+                    break;
+                case "10":
+                    MeusExerciciosCSharp.Nivel01_EntradaSaida.Exercicio10.Exercicio10.Executar();
+                    break;
+                case "11":
+                    MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio11.Exercicio11.Executar();
+                    break;
+                case "12":
+                    MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio12.Exercicio12.Executar();
+                    break;
+                case "13":
+                    MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio13.Exercicio13.Executar();
+                    break;
+                case "14":
+                    MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio14.Exercicio14.Executar();
+                    break;
+                case "15":
+                    MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio15.Exercicio15.Executar();
+                    break;
+                case "16":
+                    MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio16.Exercicio16.Executar();
+                    break;
+                case "17":
+                    MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio17.Exercicio17.Executar();
+                    break;
+                case "18":
+                    MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio18.Exercicio18.Executar();
+                    break;
+                case "19":
+                    MeusExerciciosCSharp.Nivel02_EstruturasCondicionais.Exercicio19.Exercicio19.Executar();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida ou exercício não encontrado.");
+                    break;
+            }
+
+            Console.Write("\nDeseja testar outro exercício? (S/N): ");
+            string resposta = Console.ReadLine();
+
+            continuar = (resposta.ToUpper() == "S");
+
+            Console.WriteLine("---------------------------------------------");
+
+        } while (continuar);
         Console.WriteLine("\nPrograma finalizado. Pressione qualquer tecla para sair...");
         Console.ReadKey();
     
